c3c60dd1fb799dbc988aaa47df6222846b68da64
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-08-19 15:59:02 +0100

    Up to the end of video 3

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css (webpack; this injects a <link> tag into the HTML)
+import css from './styles/style.styl';
+
+// Import Components
+import Main from './components/main';
+
+render(<Main></Main>, document.getElementById('root'));
