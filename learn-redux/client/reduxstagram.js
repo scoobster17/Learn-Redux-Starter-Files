22a4209fb812849af26cb45a7f645c6ea09370c0
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-08-22 09:21:54 +0100

    End of video 4

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -7,5 +7,20 @@ import css from './styles/style.styl';
 
 // Import Components
 import Main from './components/main';
+import Single from './components/single';
+import PhotoGrid from './components/photo-grid';
 
-render(<Main></Main>, document.getElementById('root'));
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+// use parantheses as we want to render a load of JSX
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router, document.getElementById('root'));
